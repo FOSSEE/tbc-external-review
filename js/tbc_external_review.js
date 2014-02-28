6c70e5ade428d43eda6a44a4442a36b776e47272
Jayaram Pai <rush2jrp@gmail.com>
2014-02-28 13:45:15 +0530

    added manage_comments for administration role.

--- js/tbc_external_review.js
+++ js/tbc_external_review.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+    var basePath = Drupal.settings.basePath;
+    var modPath = basePath + "tbc_external_review/";
+    /* for "tbc_external_review/comments" page */
     $book = $("#edit-book");
     $chapter = $("#edit-chapter");
     $chapter_wrapper = $("#edit-chapter-wrapper");
@@ -11,7 +15,7 @@ $(document).ready(function() {
     $book.change(function() {
         var pid = $(this).val();
         $.ajax({
-            url: "ajax/book/"+pid,
+            url: modPath + "ajax/book/"+pid,
             type: "GET",
             success: function(data) {
                 $chapter.html(data);
@@ -23,7 +27,7 @@ $(document).ready(function() {
     $chapter.change(function() {
         var cid = $(this).val();
         $.ajax({
-            url: "ajax/chapter/"+cid,
+            url: modPath + "ajax/chapter/"+cid,
             type: "GET",
             success: function(data) {
                 $example.html(data);
@@ -35,6 +39,27 @@ $(document).ready(function() {
     $example.change(function() {
         $error_wrapper.show();
         $submit.show();
+    });
+
+    /* for "tbc_external_review/manage_comments/#some_number" page */
+    $view_comment = $(".view-comment");
+    $lightbox_wrapper = $("#lightbox-wrapper");
+    $lightbox_inner = $("#lightbox-inner");
+    $view_comment.click(function(e) {
+        var comment_id = $(this).attr("data-comment");
+        
+        $.ajax({
+            url: modPath + "ajax/comment/" + comment_id,
+            type: "GET",
+            dataType: "html",
+            success: function(data) {
+                $lightbox_inner.html(data);
+                $lightbox_wrapper.lightbox_me({
+                    centered: true
+                });
+            },
+        });
+        e.preventDefault();
     });
 
     $(document).ajaxStart(function() {
